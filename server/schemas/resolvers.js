3cacc55e9ed310d02788bdb0176c3eb6d963c16a
Ankur <ankurshahi@Ankurs-iMac.local>
2021-09-13 19:07:24 -0400

    authorize users with JWT and implement auth middleware to populate Me Query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,17 +1,26 @@
 const { AuthenticationError } = require("apollo-server-errors");
 const { User } = require("../models");
+const {signToken}=require('../utils/auth');
 
 const resolvers = {
     Query: {
-        me: async()=>{
-            return User.find();
+        me: async(parent, args, context)=>{
+            if(context.user){
+                const userData = await User.findOne({_id: context.user._id})
+                    .select('-__v -password');
+            
+                return userData;
+            }
+            
+            throw new AuthenticationError('Not logged in')
         }
     },
     Mutation:{
         addUser: async(parent, args)=>{
             const user=await User.create(args);
+            const token = signToken(user);
 
-            return user;
+            return {token, user};
         },
         login: async(parent, {email, password})=>{
             const user=await User.findOne({email});
@@ -26,9 +35,9 @@ const resolvers = {
                 throw new AuthenticationError('Incorrect credentials');
             }
 
-            // const token = signToken(user);
+            const token = signToken(user);
 
-            return user;
+            return {token, user};
         }
     }
 };
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 // import ApolloServer
 const {ApolloServer}=require('apollo-server-express');
+const {authMiddleware}=require('./utils/auth');
 
 // import typeDefs and resolvers
 const {typeDefs, resolvers}=require('./schemas');
-// const path = require('path');
+const path = require('path');
 const db = require('./config/connection');
 // const routes = require('./routes');
 
@@ -13,19 +14,20 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and  pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate the Apollo server with the Express application as middleware
 server.applyMiddleware({app});
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 // app.use(routes);
 
