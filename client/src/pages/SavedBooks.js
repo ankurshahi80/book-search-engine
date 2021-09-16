cce1e30c21aff1f2ab1cdfea838d14d987971762
Ankur <ankurshahi@Ankurs-iMac.local>
2021-09-16 14:17:46 -0400

    refresh window after deleting the book

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -33,6 +33,7 @@ const SavedBooks = () => {
 
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
+      window.location.reload();
     } catch (err) {
       console.error(err);
     }
